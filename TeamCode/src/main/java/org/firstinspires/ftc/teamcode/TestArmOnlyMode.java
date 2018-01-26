f925eefce61a03f06e3fa165039b8dcbe38b6752
John Rasor <jmrasorsdb@gmail.com>
2018-01-26 12:58:55 -0800

    Simplified Sliderbot arm test, added simple Autonomous Pictograph reader

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestArmOnlyMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestArmOnlyMode.java
@@ -10,15 +10,16 @@ import com.qualcomm.robotcore.hardware.Servo;
  * class members. The arm can move the gripper fore and aft, along a circular
  * arc with high point at midway extension. The slider can move the gripper,
  * too, but does so straight fore and aft.
+ * JMR 1/24/18 v 0.2 Simplified to respond only to Driver 2's game pad right stick, y motion.
  */
 
 @TeleOp(name="Test Arm Only", group="Test")
 public class TestArmOnlyMode extends LinearOpMode {
     private Sliderbot robot = new Sliderbot(this);
-
     @Override
     public void runOpMode() {
-        // Initialization: map the motors. No other initialization.
+
+        // Initialization: map the arm motors. No other initialization.
         robot.hwMapArmMotors();
 
         telemetry.addData("Status",
@@ -28,49 +29,17 @@ public class TestArmOnlyMode extends LinearOpMode {
         //Wait for testing to start.
         waitForStart();
         //elapsedTime.reset();
+        double power = 0.0;
 
         while(opModeIsActive()) {
             // Use: set arm motor powers according to game pad.
-            /*
-            if(gamepad1.dpad_up && (armMotor1.getCurrentPosition() < 100)) {
-                armMotor1.setPower(0.25);
-                armMotor2.setPower(-0.25);
-            } else if(gamepad1.dpad_down && (armMotor1.getCurrentPosition() > 0)) {
-                armMotor1.setPower(-0.25);
-                armMotor2.setPower(0.25);
-            } else {
-                armMotor1.setPower(0.0);
-                armMotor2.setPower(0.0);
-            }*/
+            power = gamepad2.right_stick_y;
 
-            double power = 0.0;
-            double armEncoder = robot.armMotor1.getCurrentPosition();
-              // The armMotor2 will just mimic armMotor1.
-
-            if(gamepad1.y) { // yellow button, used for nudging the arm.
-                // Another use: set arm motor powers according to game pad,
-                // tempered.
-                // TODO: Make these parameters into constants.
-                if(armEncoder < -800) { // Need to apply negative power
-                    // Encoder = -800, output = 0. Encoder = -1200, output = -0.35
-                    power = Math.min(-(armEncoder + 600.0) / 400.0, 1.0) * -0.35;
-                } else { //Need to apply positive power
-                    // Encoder = -800, output = 0. Encoder = -400, output = 0.35
-                    power = Math.min((armEncoder + 600.0) / 200.0, 1.0) * 0.35;
-                }
-            } else if (gamepad1.x) {
-                if(armEncoder < -400.0)
-                    power = -0.35;
-            } else if (gamepad1.b) {
-                if(armEncoder > -800.0)
-                   power = 0.35;
-            }
             robot.armMotor1.setPower(-power);
             robot.armMotor2.setPower(power); // Arm motors face each other.
 
             telemetry.addData("Status", "RUNNING");
-            telemetry.addData("Encoder Position",
-              robot.armMotor1.getCurrentPosition());
+            telemetry.addData("Encoder Position", robot.armMotor1.getCurrentPosition());
             telemetry.update();
         }
     }
